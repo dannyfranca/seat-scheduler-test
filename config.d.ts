43c6524586949f6961d266e444d2d6323129940b
Danny Franca <me@dannyfranca.com>
2024-07-07 13:18:54 +0100

    refactor: remove redis env

--- config.d.ts
+++ config.d.ts
@@ -2,7 +2,6 @@ declare global {
   interface AppConfig {
     APP_NAME: string;
     PORT: string;
-    REDIS_URI: string;
     POSTGRES_URI: string;
   }
 }
--- src/load-configs.ts
+++ src/load-configs.ts
@@ -4,6 +4,6 @@ import { checkRequiredEnvs } from './utils/check-required-envs';
  * Depending on the runtime, environment variables might not be the safest place to store some secrets, since it can be inspected by users who have read-only access to the deployed resource. A async function to retrieve the secrets from a secret store at runtime may be desirable.
  */
 export async function loadConfigs() {
-  // POSTGRES_URI and REDIS_URI could potentially be loaded from a secret store
-  return checkRequiredEnvs<AppConfig>(['APP_NAME', 'PORT', 'POSTGRES_URI', 'REDIS_URI']);
+  // POSTGRES_URI could potentially be loaded from a secret store
+  return checkRequiredEnvs<AppConfig>(['APP_NAME', 'PORT', 'POSTGRES_URI']);
 }
