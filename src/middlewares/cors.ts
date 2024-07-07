ef4520c171b8ed62fd3de847b410c935049582f9
Danny Franca <me@dannyfranca.com>
2024-07-07 16:35:11 +0100

    fix: change next hook placement on cors middleware

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,9 +1,9 @@
 import { defineMiddleware } from '@/utils/factories';
 
 export const defineCorsMiddleware = defineMiddleware(async (c, next) => {
+  await next();
+
   c.header('Access-Control-Allow-Origin', '*');
   c.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
   c.header('Access-Control-Allow-Headers', '*');
-
-  await next();
 });
